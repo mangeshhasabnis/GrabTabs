de7f52da78399afa980bc186f728341f3d8d6bc1
mangeshhasabnis <mangesh.hasabnis@gmail.com>
2019-03-10 11:54:51 +0530

    Added author information

---
+++ background.js
@@ -0,0 +1,64 @@
+/*
+Author: Mangesh Hasabnis
+*/
+const ParentBookmarFolder = 'GrabTabs Bookmarks';
+
+chrome.browserAction.onClicked.addListener(function() {
+    CheckParentBookmarkFolderExists().then(CreateGroupBookmark);
+});
+
+function CreateGroupBookmark(bookmarks) {
+    GetRootFolder(bookmarks).then(function(parentBookmark) {
+        GetAllTabs().then(function(tabs) {
+            CreateBookmark(tabs, parentBookmark.id);
+        })
+    });
+}
+
+function GetAllTabs() {
+    return new Promise(function(resolve) {
+        chrome.tabs.query(tabQueryInfo, function(tabs) {
+            resolve(tabs);
+        });
+    })
+
+}
+
+function CreateBookmark(tabs, parentId) {
+    for(tab in tabs) {
+        console.log(tab);
+        chrome.bookmarks.create({
+            'parentId': parentId,
+            'index': parseInt(tab),
+            'title': tabs[tab].title,
+            'url': tabs[tab].url
+        });
+    }
+}
+
+function GetRootFolder(bookmarks) {
+    return new Promise(function(resolve) {
+        var bookmarkFolder;
+
+        if(bookmarks.length === 0) {
+            chrome.bookmarks.create({
+                'title': ParentBookmarFolder
+            }, function(newFolder) {
+                console.log("Added new folder" + newFolder);
+                bookmarkFolder = newFolder;
+                resolve(bookmarkFolder);
+            });
+        } else {
+            bookmarkFolder = bookmarks[0];
+            resolve(bookmarkFolder);
+        }        
+    });
+};
+
+function CheckParentBookmarkFolderExists() {
+    return new Promise(function(resolve, reject) {
+        chrome.bookmarks.search(ParentBookmarFolder, function(bookmarks) {
+            resolve(bookmarks);
+        });
+    })
+}
